4d21cf14fbe32832fd37df13d31a49c99248d08c
josorian <josejaviersoriano54@gmail.com>
2024-02-27 01:33:03 +0100

    Initial commit

---
+++ app/src/androidTest/java/com/example/cupcake/test/ScreenAssertion.kt
@@ -0,0 +1,7 @@
+package com.example.cupcake.test
+import androidx.navigation.NavController
+import org.junit.Assert
+
+fun NavController.assertCurrentRouteName(expectedRouteName: String) {
+    Assert.assertEquals(expectedRouteName, currentBackStackEntry?.destination?.route)
+}
